ab2c430736dbaee79d37bd8d022cb517ff4f45b5
GeoffBurns <geoffrey.peter.burns@gmail.com>
2021-09-23 21:44:21 +1000

    move view classes
    split package

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "PlayingCard",
     platforms: [
-          .iOS(.v14),
+          .iOS(.v11),
           .macOS(.v11)
       ],
     products: [
